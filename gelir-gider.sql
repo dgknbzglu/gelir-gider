369ae791f44a0b0faefec2703d8055949b31118f
Doğukan Bozoğlu <55700237+dgknbzglu@users.noreply.github.com>
2024-01-23 16:57:43 +0300

    Add files via upload

---
+++ gelir-gider.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Anamakine: 127.0.0.1:3306
+-- Üretim Zamanı: 23 Oca 2024, 13:55:57
+-- Sunucu sürümü: 8.0.31
+-- PHP Sürümü: 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Veritabanı: `gelir-gider`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `gelir_gider`
+--
+
+DROP TABLE IF EXISTS `gelir_gider`;
+CREATE TABLE IF NOT EXISTS `gelir_gider` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `tur` int NOT NULL,
+  `miktar` decimal(10,2) NOT NULL,
+  `tarih` date NOT NULL,
+  `kategori` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
